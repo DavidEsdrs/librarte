3dd9b6d3402438170739e351dce09c426abc8678
DavidEsdrs <carpina1983@gmail.com>
2023-06-23 17:03:18 -0300

    feat: Import Auth and Prisma modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AuthController } from './auth/auth.controller'
+import { AuthModule } from './auth/auth.module'
+import { PrismaModule } from "./prisma/prisma.module"
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, PrismaModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}

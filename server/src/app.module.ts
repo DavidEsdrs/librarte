c8d1847ada87fc52ee51bee615e809d834ab80f5
DavidEsdrs <carpina1983@gmail.com>
2023-07-06 18:27:24 -0300

    feat: Import AuthorsModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth/auth.module'
 import { PrismaModule } from "./prisma/prisma.module"
 import { BookInfoModule } from './book-info/book-info.module';
 import { BooksModule } from './books/books.module';
+import { AuthorsModule } from "./authors/authors.module"
 
 @Module({
-  imports: [AuthModule, PrismaModule, BookInfoModule, BooksModule],
+  imports: [AuthModule, PrismaModule, BookInfoModule, BooksModule, AuthorsModule],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })

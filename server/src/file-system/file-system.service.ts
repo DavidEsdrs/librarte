26873acd78a9e3dd1dea29701dd44ea7882260ca
DavidEsdrs <carpina1983@gmail.com>
2023-07-08 15:15:44 -0300

    feat: Create file-system module to manage fs operations

---
+++ server/src/file-system/file-system.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import path from 'path'
+import fs from 'fs'
+import { promisify } from 'util'
+
+const unlink = promisify(fs.unlink)
+
+@Injectable()
+export class FileSystemService {
+  private uploadedFilesName: string[] = []
+
+  async deleteFile(filename: string, filepath: string[]): Promise<boolean> {
+    try {
+      const pt = path.join("uploads", ...filepath, filename)
+      await unlink(pt)
+      return true
+    } catch {
+      return false
+    }
+  }
+
+  getUploadedFilesName(): string[] {
+    return this.uploadedFilesName
+  }
+
+  pushUploadedFile(filename: string) {
+    this.uploadedFilesName.push(filename)
+  }
+}

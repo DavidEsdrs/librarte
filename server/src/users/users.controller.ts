82ab788adec8d2f1090d0829bf6c3630ffb1c43a
DavidEsdrs <carpina1983@gmail.com>
2023-07-08 15:16:15 -0300

    feat: Create users module to manage users and their roles

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Param, Post, Body, Request, HttpCode } from '@nestjs/common';
+import { UsersService } from "./users.service";
+
+@Controller('users')
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  @Post("/:id")
+  @HttpCode(204)
+  addRole(
+    @Param("id") id: number,
+    @Body() { type }: { type: "ADMIN" | "USER" | "GUEST" },
+    @Request() request
+  ) {
+    this.service.addRole({ user_id: id, requester_id: request.sub.id, type })
+  }
+}
---
+++ server/src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async addRole({
+    user_id,
+    type,
+    requester_id
+  }: { 
+    type: "ADMIN" | "USER" | "GUEST",
+    user_id: number,
+    requester_id: number
+  }) {
+    const requester = await this.prisma.user.findUnique({
+      where: {
+        id: requester_id
+      },
+      include: {
+        roles: true
+      }
+    })
+    if(!requester) {
+      throw new UnauthorizedException()
+    }
+    const canPerformAction = requester.roles.some(r => r.type === "ADMIN")
+    if(!canPerformAction) {
+      throw new ForbiddenException()
+    }
+    await this.prisma.user.update({
+      where: {
+        id: user_id
+      },
+      data: {
+        roles: {
+          create: [ { type } ]
+        }
+      }
+    })
+  }
+}

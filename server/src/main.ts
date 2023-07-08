d8fa7fd20347602c82bd1d4b8b6d8fb77ba46595
DavidEsdrs <carpina1983@gmail.com>
2023-07-08 03:09:27 -0300

    feat: Use ValidationPipe's exceptionFactory to throw custom error messages with status code 422

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -8,7 +8,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     forbidNonWhitelisted: true,
-    forbidUnknownValues: true
+    forbidUnknownValues: true,
+    exceptionFactory: (errors) => {
+      errors.forEach(err => console.log({err}))
+      const result = errors.map((error) => ({
+        property: error.property,
+        message: error.constraints[Object.keys(error.constraints)[0]],
+      }))
+      return new UnprocessableEntityException(result)
+    }
   }))
   await app.listen(3000)
 }

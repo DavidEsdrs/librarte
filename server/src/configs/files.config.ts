b8eb7239d210af314a80cc718f55f828454d0ccb
DavidEsdrs <carpina1983@gmail.com>
2023-07-08 15:21:24 -0300

    feat: Create configuration file to manage books upload

---
+++ server/src/configs/files.config.ts
@@ -0,0 +1,34 @@
+import { Provider } from '@nestjs/common';
+import { extname } from "path";
+import { imageFileFilter } from "src/common/utils/utils";
+
+export const filesConfigProvider: Provider = {
+  provide: 'FILES_CONFIG_PROVIDER',
+  useValue: [
+    {
+      field: "cover",
+      fileFilter: imageFileFilter,
+      dest: './uploads/books/cover',
+      editFileName(req, file, callback) {
+        const fileExtName = extname(file.originalname);
+        const name = Date.now();
+        const fileName = `cover-${name}${fileExtName}`;
+        callback(null, fileName);
+        return fileName;
+      },
+    },
+    {
+      field: "featured_images",
+      maxCount: 5,
+      fileFilter: imageFileFilter,
+      dest: './uploads/books/featuredImages',
+      editFileName(req, file, callback) {
+        const fileExtName = extname(file.originalname);
+        const name = Date.now();
+        const fileName = `featured_images-${name}${fileExtName}`;
+        callback(null, fileName);
+        return fileName;
+      },
+    },
+  ],
+};

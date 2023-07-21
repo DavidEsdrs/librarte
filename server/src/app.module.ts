9f0209d4bff3f06b7d306f7cd5c74f415bd7ed97
DavidEsdrs <carpina1983@gmail.com>
2023-07-20 21:53:17 -0300

    feat: Add isOpen column is posts table

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,9 +7,12 @@ import { PrismaModule } from "./prisma/prisma.module"
 import { BookInfoModule } from './book-info/book-info.module';
 import { BooksModule } from './books/books.module';
 import { AuthorsModule } from "./authors/authors.module"
+import { PostsModule } from './posts/posts.module';
+import { ProposalsModule } from "./proposals/proposals.module"
+import { DealsModule } from './deals/deals.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BookInfoModule, BooksModule, AuthorsModule],
+  imports: [AuthModule, PrismaModule, BookInfoModule, BooksModule, AuthorsModule, PostsModule, ProposalsModule, DealsModule],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })

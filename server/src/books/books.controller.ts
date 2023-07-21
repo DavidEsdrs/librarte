433b5ce9e6fafd59c14c6569bbcb6d619c1829c3
DavidEsdrs <carpina1983@gmail.com>
2023-07-20 22:07:56 -0300

    feat(createBook): Use CreateBookPipe

--- server/src/books/books.controller.ts
+++ server/src/books/books.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, UseInterceptors, Request, UploadedFiles, Get, Param, UseFilters, ParseFilePipe } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors, Request, UploadedFiles, Get, Param, UseFilters, UsePipes } from '@nestjs/common';
 import { CreateBookDTO } from './dto/books.dto'
 import { BooksService } from './books.service'
 import { FilesFieldsInterceptor } from 'src/common/interceptors/files-fields.interceptor'
 import { UnprocessableEntityExceptionFilter } from "src/common/filters/unprocessable-entity-exception.filter";
 import { FileSystemService } from "src/file-system/file-system.service";
+import { CreateBookPipe } from "./pipes/createBook.pipe";
 
 @Controller('books')
 export class BooksController {
@@ -12,6 +13,7 @@ export class BooksController {
   @Post("/")
   @UseInterceptors(FilesFieldsInterceptor)
   @UseFilters(UnprocessableEntityExceptionFilter)
+  @UsePipes(CreateBookPipe)
   async createBook(
     @Body() createBookDto: CreateBookDTO,
     @Request() request,

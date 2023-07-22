aefb85052c7d40f825b059926d28dd830cb5c838
DavidEsdrs <carpina1983@gmail.com>
2023-07-22 05:27:54 -0300

    feat: Import deals and chats modules

--- server/src/proposals/proposals.module.ts
+++ server/src/proposals/proposals.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProposalsController } from './proposals.controller';
 import { ProposalsService } from './proposals.service';
+import { DealsModule } from "src/deals/deals.module";
+import { ChatsModule } from "src/chats/chats.module";
 
 @Module({
   controllers: [ProposalsController],
-  providers: [ProposalsService]
+  providers: [ProposalsService],
+  imports: [DealsModule, ChatsModule]
 })
 export class ProposalsModule {}

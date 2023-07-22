ded4133195ea38b7a8b265a4bf0c666f0f26b444
DavidEsdrs <carpina1983@gmail.com>
2023-07-22 01:58:17 -0300

    feat: Add global exception filter

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common'
+import { HttpExceptionFilter } from "./common/filters/http-exception.filter"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+  app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     forbidNonWhitelisted: true,
     forbidUnknownValues: true,
     exceptionFactory: (errors) => {
-      errors.forEach(err => console.log({err}))
       const result = errors.map((error) => ({
         property: error.property,
         message: error.constraints[Object.keys(error.constraints)[0]],

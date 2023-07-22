d66ce18437f1b0ef62c375caa9db426f77280795
DavidEsdrs <carpina1983@gmail.com>
2023-07-22 05:29:37 -0300

    feat: Create IsBeforeNow custom validator

---
+++ server/src/common/decorators/is-before-now.decorator.ts
@@ -0,0 +1,23 @@
+import { ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments, ValidationOptions, registerDecorator } from "class-validator";
+
+@ValidatorConstraint()
+export class IsBeforeNowConstraint implements ValidatorConstraintInterface {
+  validate(date: number) {
+    return date <= new Date().getFullYear();
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `Date ${args.property} has to be equals or less than current year.`;
+  }
+}
+
+export function IsBeforeNow(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: IsBeforeNowConstraint,
+    });
+  };
+}

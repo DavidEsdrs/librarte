3801872313f205b126efe7766ec83354594e336a
DavidEsdrs <carpina1983@gmail.com>
2023-07-22 18:39:10 -0300

    fix: Get the front end url from .env

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: '*',
+    origin: process.env.FRONT_END_URL,
     credentials: true,
   })
   app.use(cookieParser())
